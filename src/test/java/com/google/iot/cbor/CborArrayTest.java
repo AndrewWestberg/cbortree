55ce7802bb40922584b3ded1713a3e4fae06866f
Andrew Westberg <andrewwestberg@gmail.com>
2022-01-01 01:54:22 +0000

    fix test that expected to re-encode arrays differently

--- src/test/java/com/google/iot/cbor/CborArrayTest.java
+++ src/test/java/com/google/iot/cbor/CborArrayTest.java
@@ -203,7 +203,8 @@ public class CborArrayTest extends CborTestBase {
     void testCborArrayJavaObjectConversion() throws Exception {
         byte[] array = decode("9F0102030405060708090a0b0c0d0e0f101112131415161718181819FF");
 
-        byte[] recodedArray = decode("98190102030405060708090a0b0c0d0e0f101112131415161718181819");
+//        byte[] recodedArray = decode("98190102030405060708090a0b0c0d0e0f101112131415161718181819");
+        byte[] recodedArray = decode("9F0102030405060708090a0b0c0d0e0f101112131415161718181819FF");
 
         String output = "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]";
 
